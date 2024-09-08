86ec1dfdf401fd7d4405c2a6f1e6f49dd1a62196
Matheus Accorsi <matheus@token.com.br>
2024-09-08 01:03:03 -0300

    GET Game by Id

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,10 +1,6 @@
-package com.devsuperior.dslist;
+package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 public class GameMinDTO {
 
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 //service é um componente do sistema, tem que ser registrado, para o framework comentar
 
-import com.devsuperior.dslist.GameMinDTO;
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.devsuperior.dslist.entities.Game;
@@ -16,6 +18,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
     // consulta por padrão no banco de dados, feito de maneira automatica pelo framework
+
+    public GameDTO findById(Long id){
+       Game result = gameRepository.findById(id).get();
+        GameDTO  dto = new GameDTO(result);
+        return dto;
+
+
+    }
 
     public List<GameMinDTO> findAll(){
         List<Game> result= gameRepository.findAll();
